f7ad3a89821436005080eb581dcb40ef49d076a4
twsss111 <twsss111@hotmail.com>
2015-02-21 11:15:30 +0800

    updated the R program with comments in the code

--- cachematrix.R
+++ cachematrix.R
@@ -6,18 +6,31 @@
 ## can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-	m_inv <- NULL
-        set <- function(y) {
-                x <<- y
-                m_inv <<- NULL
-        }
-        get <- function() x
-        setinverse <- function(solve) m_inv <<- solve
-        getinverse <- function() m_inv
-        list(set = set, get = get,
-             setinverse = setinverse,
-             getinverse = getinverse)
+	
+	cache_inv <- NULL
+	# cache_inv is used to cache the inversed Matrix
+	# initially nothing is cached so set it to NULL
+	
+	set <- function(y) {
+		x <<- y
+		cache_inv <<- NULL
+		}
+	#store the new matrix value and reset the cache_inv to NULL
 
+	get <- function() x
+	#show the stored matrix value
+
+	setinverse <- function(solve) cache_inv <<- solve
+	#store the inversed Matrix value to cache_inv
+
+	getinverse <- function() cache_inv
+	#show the inversed matrix value	
+
+	list(	set = set, 
+		get = get,
+		setinverse = setinverse,
+		getinverse = getinverse)
+	#show a list that each element is a function
 }
 
 
@@ -27,15 +40,26 @@ makeCacheMatrix <- function(x = matrix()) {
 ## should retrieve the inverse from the cache.
 
 cacheSolve <- function(y, ...) {
-        ## Return a matrix that is the inverse of 'x'
 	m_inv <- y$getinverse()
-        if(!is.null(m_inv)) {
-                message("getting cached data")
-                return(m_inv)
-        }
-        data <- y$get()
-        m_inv <- solve(data, ...)
-        y$setinverse(m_inv)
-        m_inv
+	#first get the stored inversed matrix value
+	
+	if(!is.null(m_inv)) {
+		message("getting cached data")
+		return(m_inv)
+		}
+	#if stored inversed matrix value is not null, 
+	#show the message "getting cached data"
+	
+	data <- y$get()
+	#If the stored inversed matrix value is null, get the matrix value first
+	
+	m_inv <- solve(data, ...)
+	#then calculate the inversed matrix value
+	#and store the value to m_inv
 
+	y$setinverse(m_inv)
+	#also set the inversed matrix to the cache_inv
+
+	m_inv
+	#show the inversed matrix value
 }
